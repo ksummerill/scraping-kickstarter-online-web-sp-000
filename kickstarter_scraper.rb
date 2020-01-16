9036724bdf0edda6f209dc3a1dd5c7f1cbb532ba
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-16 02:46:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,10 +2,6 @@
 require 'nokogiri'
 require 'pry'
 
-html = File.read('fixtures/kickstarter.html')
-
-kickstarter = Nokogiri::HTML(html)
-
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
