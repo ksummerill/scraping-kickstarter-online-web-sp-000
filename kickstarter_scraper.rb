62e8aee77946abfab422c79db845c171e6aea910
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-16 02:44:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ kickstarter = Nokogiri::HTML(html)
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta").text
+# % funded: project.css("ul.project-stats li.first.funded strong").text
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
