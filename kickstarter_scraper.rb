4621d6da9a8acda1a3dcc5ba5fed0bc3efec6584
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-16 02:34:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # require libraries/modules here
 require 'nokogiri'
+require 'pry'
 
 html = File.read('fixtures/kickstarter.html')
 
@@ -8,3 +9,6 @@ kickstarter = Nokogiri::HTML(html)
 def create_project_hash
   # write your code here
 end
+
+
+binding.pry
